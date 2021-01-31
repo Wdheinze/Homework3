f894c4172359baacc796325c2165bb032a7296a1
Wdheinze <77468808+Wdheinze@users.noreply.github.com>
2021-01-31 16:23:11 -0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function update(choice){
+    var starter=document.getElementById("start");
+    var choiceOne=document.getElementById("1");
+    var choiceTwo=document.getElementById("2");
+}
+if (update==1 && update == "1"){
+    document.getElementById("start").innerHTML= "You go outside what shall you do now?"
+    document.getElementById("1").innerHTML= "Go Fishing"
+    document.getElementById("2").innerHTML="Go jogging"
+}
