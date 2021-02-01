72b7fe3936f089c9a0f73453b392fa83dbb05af2
Wdheinze <77468808+Wdheinze@users.noreply.github.com>
2021-02-01 15:07:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,17 @@ function update(choices) {
 
     var button1 = document.getElementById("1").innerHTML;
     var button2 = document.getElementById("2").innerHTML;
+
+    console.log(button1);
+    if (choices == 1 && button1 == "<h2>Go Outside</h2>") {
+        document.getElementById("start").innerHTML = "<h1>You go outside what shall you do now?</h1>"
+        document.getElementById("1").innerHTML = "<h2>Go Fishing</h2>"
+        document.getElementById("2").innerHTML = "<h2>Go jogging</h2>"
+    }
+    if (choices == 2 && button2 == "<h2>Stay Indoors</h2>") {
+        document.getElementById("start").innerHTML = "<h1>What to do now?</h1>"
+        document.getElementById("1").innerHTML = "<h2>Play Video Games</h2>"
+        document.getElementById("2").innerHTML = "<h2>Cook something to eat</h2>"
+    }
 }
-if (choices == 1 && update == "1") {
-    document.getElementById("start").innerHTML = "You go outside what shall you do now?"
-    document.getElementById("1").innerHTML = "Go Fishing"
-    document.getElementById("2").innerHTML = "Go jogging"
-}
+
