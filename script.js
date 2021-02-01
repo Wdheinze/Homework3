45320e5c3e670a6eee46e0df8a844f2d87eb26e7
Wdheinze <77468808+Wdheinze@users.noreply.github.com>
2021-01-31 22:36:00 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-function update(choices)
-{
+function update(choices) {
 
-    var button1=document.getElementById("1").innerHTML;
-    var button2=document.getElementById("2").innerHTML;
+    var button1 = document.getElementById("1").innerHTML;
+    var button2 = document.getElementById("2").innerHTML;
 }
-if (choices == 1 && update == "1")
-{
-    document.getElementById("start").innerHTML= "You go outside what shall you do now?"
-    document.getElementById("1").innerHTML= "Go Fishing"
-    document.getElementById("2").innerHTML="Go jogging"
+if (choices == 1 && update == "1") {
+    document.getElementById("start").innerHTML = "You go outside what shall you do now?"
+    document.getElementById("1").innerHTML = "Go Fishing"
+    document.getElementById("2").innerHTML = "Go jogging"
 }
